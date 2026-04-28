2ec57b85f9118847a2ed79f822b59cd474600493
Peter Bastian <peter.bastian@iwr.uni-heidelberg.de>
2026-04-28 12:44:40 +0200

    remove blocked example.

--- matvec/matvec.cc
+++ matvec/matvec.cc
@@ -3,7 +3,7 @@
 #include <string>
 #include "time_experiment.hh"
 
-// y = Ax, matrix assumed to be row major
+// y = Ax, matrix accessed consecutively, reuse on y (A is stored row-major)
 void matvec1 (int n, const double* A, const double* x, double* y)
 {
   for (int i=0; i<n; i++)
@@ -14,7 +14,7 @@ void matvec1 (int n, const double* A, const double* x, double* y)
     }
 }
 
-// y = Ax, matrix assumed to be column major now
+// y = Ax, matrix accessed consecutively and reuse on y (A is stored column-major)
 void matvec2 (int n, const double* A, const double* x, double* y)
 {
   for (int i=0; i<n; i++) y[i] = 0.0;
@@ -24,9 +24,10 @@ void matvec2 (int n, const double* A, const double* x, double* y)
   }
 }
 
-// y = Ax, matrix assumed to be column major now
+// y = Ax, blocked variant of column-major variant (A accessed consecutively, reuse on x)
 void matvec3 (int n, int b, const double* A, const double* x, double* y)
 {
+  const int B=8;
   if (n%b!=0) {
     std::cout << n << " is not a multiple of " << b << std::endl;
     exit(0);
@@ -35,13 +36,17 @@ void matvec3 (int n, int b, const double* A, const double* x, double* y)
     std::cout << b << " is not a multiple of " << 4 << std::endl;
     exit(0);
   }
+  if (b%B!=0) {
+    std::cout << b << " is a multiple of B" << std::endl;
+    exit(0);
+  }
   for (int i=0; i<n; i++)
     y[i] = 0.0;
-  for (int I=0; I<n; I+=b)
-    for (int J=0; J<n; J+=b)
-      for (int j=J; j<J+b; ++j)
-	for (int i=I; i<I+b; ++i)
-	  y[i] += A[j*n+i]*x[j];
+  for (int I=0; I<n; I+=B)
+    for (int J=0; J<n; J+=B)
+      for (int j=J; j<J+B; ++j)
+	      for (int i=I; i<I+B; ++i)
+	        y[i] += A[j*n+i]*x[j];
 }
 
 // initialize an array
@@ -159,7 +164,7 @@ public:
 int main (int argc, char** argv)
 {
   std::vector<int> sizes; // vector with problem sizes to try
-  for (int i=32; i<=25000; i*=2) sizes.push_back(i);
+  for (int i=48; i<=25000; i*=2) sizes.push_back(i);
 
   std::vector<std::string> expnames; // name of experiment
 
@@ -187,18 +192,18 @@ int main (int argc, char** argv)
       bandwidth2.push_back(result);
       std::cout << result << std::endl;
     }
-  // experiment 3
-  expnames.push_back("blocked-column-major-64x64");
-  std::cout << expnames.back() << std::endl;
-  std::vector<double> bandwidth3;
-  for (auto n : sizes)
-    { 
-      Experiment3 e(n,64);
-      auto d = time_experiment(e);
-      double result = d.first*e.operations()/d.second/1e9;
-      bandwidth3.push_back(result);
-      std::cout << result << std::endl;
-    }
+  // // experiment 3
+  // expnames.push_back("blocked-column-major-64x64");
+  // std::cout << expnames.back() << std::endl;
+  // std::vector<double> bandwidth3;
+  // for (auto n : sizes)
+  //   { 
+  //     Experiment3 e(n,48);
+  //     auto d = time_experiment(e);
+  //     double result = d.first*e.operations()/d.second/1e9;
+  //     bandwidth3.push_back(result);
+  //     std::cout << result << std::endl;
+  //   }
 
   // output results
   // Note: size of TLB mentioned in https://www.realworldtech.com/haswell-cpu/5/
@@ -211,7 +216,7 @@ int main (int argc, char** argv)
       std::cout << sizes[i];
       std::cout << ", " << bandwidth1[i];
       std::cout << ", " << bandwidth2[i];
-      std::cout << ", " << bandwidth3[i];
+//      std::cout << ", " << bandwidth3[i];
       std::cout << std::endl;
     }
   
