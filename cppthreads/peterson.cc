577fe28de3fc34a351cec44e1f74677517d43b7e
Peter Bastian <peter.bastian@iwr.uni-heidelberg.de>
2026-05-13 11:25:39 +0200

    added peterson algorithm from legacy

--- cppthreads/peterson.cc
+++ cppthreads/peterson.cc
@@ -9,8 +9,7 @@ int in1=0;   // indicates that thread 1 is in critical section
 int in2=0;   // indicates that thread 2 is in critical section
 int last=1;  // process that arrived last at critical section
 int count=0; // counts how many times the critical section was passed
-std::atomic<int> alast{1};
-std::atomic_flag f{ATOMIC_FLAG_INIT};
+std::atomic_flag f; //{ATOMIC_FLAG_INIT};
 std::atomic<int> acount{0};
 
 void P1_nolock ()
@@ -27,6 +26,7 @@ void P2_nolock ()
 
 void P1_peterson ()
 {
+  //std::this_thread::sleep_for(std::chrono::milliseconds(500));
   for (int i=0; i<n; i++)
     {
       in1 = 1;     // I want to enter
@@ -44,32 +44,6 @@ void P2_peterson ()
       in2 = 1;     // I want to enter
       last = 2;    // tie breaker
       while (in1 && last==2) ; // busy wait
-      count += 1;  // increment counter in c.s.
-      in2 = 0;     // we are done
-    }
-}
-
-void P1_peterson_fenced ()
-{
-  for (int i=0; i<n; i++)
-    {
-      in1 = 1;     // I want to enter
-      alast.store(1);    // tie breaker
-      std::atomic_thread_fence(std::memory_order_release);
-      while (in2 && alast.load()==1) ;
-      count += 1;  // increment counter in c.s.
-      in1 = 0;     // we are done
-    }
-}
-
-void P2_peterson_fenced ()
-{
-  for (int i=0; i<n; i++)
-    {
-      in2 = 1;     // I want to enter
-      alast.store(2);    // tie breaker
-      std::atomic_thread_fence(std::memory_order_release);
-      while (in1 && alast.load()==2) ;
       count += 1;  // increment counter in c.s.
       in2 = 0;     // we are done
     }
@@ -129,17 +103,6 @@ int main ()
   d = stop-start;
   duration = std::chrono::duration_cast<std::chrono::microseconds>(d).count();
   std::cout << "peterson       : count is " << count << " and should be " << 2*n << " duration " << (1.0*duration)/n << " us" << std::endl;
-
-  count=0;
-  start = std::chrono::high_resolution_clock::now();
-  std::thread t1c{P1_peterson_fenced};
-  std::thread t2c{P2_peterson_fenced};
-  t1c.join();
-  t2c.join();
-  stop = std::chrono::high_resolution_clock::now();
-  d = stop-start;
-  duration = std::chrono::duration_cast<std::chrono::microseconds>(d).count();
-  std::cout << "peterson_fenced: count is " << count << " and should be " << 2*n << " duration " << (1.0*duration)/n << " us" << std::endl;
 
   count=0;
   start = std::chrono::high_resolution_clock::now();
